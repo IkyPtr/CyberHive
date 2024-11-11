e0e796630d29015d2de46a5e4fb61baa0f71ece8
aqilawafiah <aqila23ti@mahasiswa.pcr.ac.id>
2024-11-11 11:44:58 +0700

    tes aqila

---
+++ TEST/resources/views/indexH.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <td>No</td>
+            <td>Nama</td>
+            <td>Motto</td>
+        </tr>
+        <tr>
+            <td>1.</td>
+            <td>Muhammad Abyaz Shadiq</td>
+            <td>Jadilah pengikut impianmu, bukan pengikut keinginan orang lain</td>
+        </tr>
+        <tr>
+            <td>2.</td>
+            <td></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td>3.</td>
+            <td>Aqila Wafiah</td>
+            <td>Aku cantik</td>
+        </tr>
+        <tr>
+            <td>4.</td>
+            <td>Helfia Giska Renata</td>
+            <td>Jalani aja dulu</td>
+        </tr>
+        <tr>
+            <td>4.</td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+</body>
+</html>
