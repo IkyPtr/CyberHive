06a6327ebf4db661b06c88f4360d319248fa5ab4
Teguh <teguh23ti@mahasiswa.pcr.ac.id>
2024-12-02 11:05:30 +0700

    Pembuatan view peserta

---
+++ TEST/resources/views/Peserta/peserta_create.blade.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="keywords"
+        content="creative tim, updivision, html dashboard, laravel, html css dashboard laravel, soft ui dashboard laravel, laravel soft ui dashboard, soft ui admin, laravel dashboard, laravel admin, web dashboard, bootstrap 5 dashboard laravel, bootstrap 5, css3 dashboard, bootstrap 5 admin laravel, soft ui dashboard bootstrap 5 laravel, frontend, responsive bootstrap 5 dashboard, soft ui dashboard, soft ui laravel bootstrap 5 dashboard" />
+    <meta name="description"
+        content="A free Laravel Dashboard featuring dozens of UI components & basic Laravel CRUDs." />
+    <meta itemprop="name" content="Soft UI Dashboard Laravel by Creative Tim & UPDIVISION" />
+    <meta itemprop="description"
+        content="A free Laravel Dashboard featuring dozens of UI components & basic Laravel CRUDs." />
+    <meta itemprop="image"
+        content="https://s3.amazonaws.com/creativetim_bucket/products/602/original/soft-ui-dashboard-laravel.jpg" />
+    <meta name="twitter:card" content="product" />
+    <meta name="twitter:site" content="@creativetim" />
+    <meta name="twitter:title" content="Soft UI Dashboard Laravel by Creative Tim & UPDIVISION" />
+    <meta name="twitter:description"
+        content="A free Laravel Dashboard featuring dozens of UI components & basic Laravel CRUDs." />
+    <meta name="twitter:creator" content="@creativetim" />
+    <meta name="twitter:image"
+        content="https://s3.amazonaws.com/creativetim_bucket/products/602/original/soft-ui-dashboard-laravel.jpg" />
+    <meta property="fb:app_id" content="655968634437471" />
+    <meta property="og:title" content="Soft UI Dashboard Laravel by Creative Tim & UPDIVISION" />
+    <meta property="og:type" content="article" />
+    <meta property="og:url" content="https://www.creative-tim.com/live/soft-ui-dashboard-laravel" />
+    <meta property="og:image"
+        content="https://s3.amazonaws.com/creativetim_bucket/products/602/original/soft-ui-dashboard-laravel.jpg" />
+    <meta property="og:description"
+        content="A free Laravel Dashboard featuring dozens of UI components & basic Laravel CRUDs." />
+    <meta property="og:site_name" content="Creative Tim" />
+
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('public/assets/img/apple-icon.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('public/assets/img/favicon.png') }}">
+    <title>
+        PROJEK LARAVEL CYBERHIVE KELOMPOK 3 BPF
+    </title>
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+    <!-- Nucleo Icons -->
+    <link href="{{ asset('public/assets/css/nucleo-icons.css') }}" rel="stylesheet" />
+    <link href="{{ asset('public/assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <link href="{{ asset('public/assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+    <!-- CSS Files -->
+    <link id="pagestyle" href="{{ asset('public/assets/css/soft-ui-dashboard.css?v=1.0.3') }}" rel="stylesheet" />
+</head>
+
+<body class="g-sidenav-show  bg-gray-100  ">
+
+
+    <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 "
+        id="sidenav-main">
+        <div class="sidenav-header">
+            <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
+                aria-hidden="true" id="iconSidenav"></i>
+            <a class="align-items-center d-flex m-0 navbar-brand text-wrap" href="">
+                <img src="{{ asset('public/assets/img/pcr (2).png') }}" class="navbar-brand-img h-100" alt="...">
+
+            </a>
+        </div>
+
+    </aside>
+    @extends('layouts.user_type.guest')
+
+@section('content')
+
+  <main class="main-content  mt-0">
+    <section>
+      <div class="page-header min-vh-75">
+        <div class="container">
+          <div class="row">
+            <div class="col-xl-6 col-lg-7 col-md-8 d-flex flex-column mx-auto">
+              <div class="card card-plain mt-8">
+                <div class="card-header pb-0 text-left bg-transparent">
+                  <h3 class="font-weight-bolder text-info text-gradient">Logbook Jurusan Teknologi Informasi</h3>
+                </div>
+                <div class="card-header pb-0 text-left bg-transparent">
+                    <h5 class="font-weight-bolder text-info text-gradient">Silahkan melakukan pengisian logbook sebelum kegiatan dilakukan</h5>
+                  </div>
+                <div class="card-body">
+                    <table>
+                        <tr>
+                            <td>
+                                <div class="text-center">
+                                    <a href="\logbookp" type="submit" class="btn bg-gradient-info w-100 mt-4 mb-0">Pinjam Lab</a>
+                                  </div>
+                            </td>
+                            <td>
+                                <div class="text-center">
+                                    <a href="\logbookp" type="submit" class="btn bg-gradient-info w-100 mt-4 mb-0">Logbook Koordinator</a>
+                                  </div>
+                                </td>
+                            <td>
+                                <div class="text-center">
+                                    <a href="\logbookp" type="submit" class="btn bg-gradient-info w-100 mt-4 mb-0">Logbook Peserta</a>
+                                  </div>
+                            </td>
+                        </tr>
+                    </table>
+              </div>
+              </div>
+
+
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+
+@endsection
+    <!--   Core JS Files   -->
+    <script src="{{ asset('public/assets/js/core/popper.min.js') }}"></script>
+    <script src="{{ asset('public/assets/js/core/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('public/assets/js/plugins/perfect-scrollbar.min.js') }}"></script>
+    <script src="{{ asset('public/assets/js/plugins/smooth-scrollbar.min.js') }}"></script>
+    <script src="{{ asset('public/assets/js/plugins/fullcalendar.min.js') }}"></script>
+    <script src="{{ asset('public/assets/js/plugins/chartjs.min.js') }}"></script>
+    <script>
+        var win = navigator.platform.indexOf('Win') > -1;
+        if (win && document.querySelector('#sidenav-scrollbar')) {
+            var options = {
+                damping: '0.5'
+            }
+            Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+        }
+    </script>
+
+    <!-- Github buttons -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+    <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
+    <script src="{{ asset('public/assets/js/soft-ui-dashboard.min.js?v=1.0.3') }}"></script>
+    <script defer src="https://static.cloudflareinsights.com/beacon.min.js/vcd15cbe7772f49c399c6a5babf22c1241717689176015"
+        integrity="sha512-ZpsOmlRQV6y907TI0dKBHq9Md29nnaEIPlkf84rnaERnq6zvWvPUqr2ft8M1aS28oN72PdrCzSjY4U6VaAw1EQ=="
+        data-cf-beacon='{"rayId":"8e347c7d6b3bce56","serverTiming":{"name":{"cfExtPri":true,"cfL4":true,"cfSpeedBrain":true,"cfCacheStatus":true}},"version":"2024.10.5","token":"1b7cbb72744b40c580f8633c6b62637e"}'
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
