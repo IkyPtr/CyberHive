626c8e4f41ec5c20a7d4f69172f692a125a10555
ikyptr <rezkisaputra14022@gmail.com>
2025-01-03 09:51:12 +0700

    selesai

--- TEST/resources/views/Admin/peserta_index.blade.php
+++ TEST/resources/views/Admin/peserta_index.blade.php
@@ -54,7 +54,7 @@
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
                     <li class="breadcrumb-item text-sm">
-                        <a class="opacity-5 text-dark" href="javascript:;">Index Koordinator</a>
+                        <a class="opacity-5 text-dark" href="javascript:;">Index Peserta</a>
                     </li>
                     <li class="breadcrumb-item text-sm text-dark active text-capitalize" aria-current="page">
                         CyberHive
